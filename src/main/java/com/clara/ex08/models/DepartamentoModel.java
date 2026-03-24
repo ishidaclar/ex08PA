097eadb1c6457245c094882f88fd9c07097a715c
ishidaclar <cishidadias@gmail.com>
2026-03-23 21:42:27 -0300

    model criado

---
+++ src/main/java/com/clara/ex08/models/DepartamentoModel.java
@@ -0,0 +1,42 @@
+package com.clara.ex08.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="tb_fornecedor")
+
+public class DepartamentoModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "Nome")
+    private String nome;
+    @Column(name = "Localizacao")
+    private String localizacao;
+
+    public DepartamentoModel() {
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+}
